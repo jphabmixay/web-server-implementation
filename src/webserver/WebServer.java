68fc7c61a32e68504005494176ffa93b1912174c
Ivan <villamar.ivan@yahoo.com>
2018-02-14 03:17:03 -0800

    changes the file path for finding/opening the corresponding file inside conf package

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -1,4 +1,5 @@
-import java.util.*;
+package webserver;
+
 import java.lang.*;
 import java.io.*;
 import java.net.ServerSocket;
@@ -6,20 +7,20 @@ import java.net.ServerSocket;
 public class WebServer 
 {
         private static final int DEFAULT_PORT = 8080;
-        private HttpdConf configuration = new HttpdConf("../conf/httpd.conf");
-        private MimeTypes mimeTypes = new MimeTypes("../conf/mime.types");
-        //private ServerSocket socket;
+        private HttpdConf configuration = new HttpdConf("src/conf/httpd.conf");
+        private MimeTypes mimeTypes = new MimeTypes("src/conf/mime.types");
+        private ServerSocket socket;
         //private Dictionary accessFiles
         
         public void start() throws IOException
         {
-            mimeTypes.load();// checking mimetypes.java
-            ServerSocket socket = new ServerSocket(DEFAULT_PORT);
-            System.out.println("Java WebServer, Starting on port: " + DEFAULT_PORT);
-            /*
+            //mimeTypes.load(); // Debug statement
+
+            socket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("Java webserver.WebServer, Starting on port: " + DEFAULT_PORT);
             while (true){
                 //listen for requests
-            } */
+            }
         }
 
         public static void main(String args[]) throws IOException
