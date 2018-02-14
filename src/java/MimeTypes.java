dfa2a162d67c82b1d9fe1decc84fd21058c20e7e
Joe Phabmixay <jphabmixay@gmail.com>
2018-02-13 18:27:54 -0800

    Added Request.java. Begun parsing HTTPrequests (still need headers and proper testing).

--- src/java/MimeTypes.java
+++ src/java/MimeTypes.java
@@ -1,9 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class MimeTypes extends ConfigurationReader{
 
     private Map<String, String> types;
 
     public MimeTypes(String fileName)
     {
+        super(fileName);
         types = new HashMap<String, String>();
     }
 
--- src/java/WebServer.java
+++ src/java/WebServer.java
@@ -7,13 +7,13 @@ public class WebServer
 {
         private static final int port = 8080; //default port
         HttpdConf configuration = new HttpdConf("../conf/httpd.conf");
-        //MimeTypes mimeTypes;
-        //ServerSocket socket;
+        MimeTypes mimeTypes = new MimeTypes("../conf/mime.types");
+        ServerSocket socket;
         //Dictionary accessFiles
         
         public void start() throws IOException
         {
-            ServerSocket socket = new ServerSocket(port);
+            socket = new ServerSocket(port);
             System.out.println("Java WebServer, Starting on port: " + port);
             while (true){
                 //listen for requests
