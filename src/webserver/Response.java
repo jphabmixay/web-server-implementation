a2de21653c9d9ac2511ae1283dd8b89ab7caeac3
Ivan <villamar.ivan@yahoo.com>
2018-02-15 01:34:51 -0800

    creates skeleton in accordance to class diagram

--- src/webserver/Response.java
+++ src/webserver/Response.java
@@ -1,4 +1,19 @@
 package webserver;
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class Response {
+    public int code;
+    public String reasonPhrase;
+    public Resource resource;
+
+    public Response(Resource resource) {
+
+    }
+
+    public void send(OutputStream out) {
+
+    }
 }
