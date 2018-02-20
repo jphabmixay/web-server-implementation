c4b02814ba4fb4822113f293bf18c61dc4560123
Joe Phabmixay <jphabmixay@gmail.com>
2018-02-19 22:23:31 -0800

    Fixed date formatting for Logger.java

--- src/webserver/Logger.java
+++ src/webserver/Logger.java
@@ -1,9 +1,12 @@
 package webserver;
 
 import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+
 
 public class Logger {
     File file;
@@ -15,10 +18,12 @@ public class Logger {
     public void write(Request request, Response response) throws IOException{
         InetAddress IP = InetAddress.getLocalHost();
         Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = " [" + dateFormat.format(date) + "] \"";
         message =   IP.getHostAddress()
                     + " - "
                     + IP.getHostName()
-                    + " [" + date + "] \""
+                    + strDate
                     + request.getVerb()
                     + " "
                     + request.getUri()
